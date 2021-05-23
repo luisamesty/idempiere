0c468c066fd19cbc1e10837f0ab97c96ff192bd5
Luis Amesty <luisamesty@gmail.com>
2021-05-23 12:14:36 +0200

    Amerpsoft maste updated 2021-05-23

--- migration/Migration_Mac_OS/Seed_82z.sh
+++ migration/Migration_Mac_OS/Seed_82z.sh
@@ -1,5 +1,5 @@
-./migrate_postgresql_for_mac.sh ../i7.1 commit | /Library/PostgreSQL/12/bin/psql -p 5434  -d idempiereSeed8.2 > Seed/Seed_7.1.lst
-./migrate_postgresql_for_mac.sh ../i7.1z commit | /Library/PostgreSQL/12/bin/psql -p 5434  -d idempiereSeed8.2 > Seed/Seed_7.1z.lst
+#./migrate_postgresql_for_mac.sh ../i7.1 commit | /Library/PostgreSQL/12/bin/psql -p 5434  -d idempiereSeed8.2 > Seed/Seed_7.1.lst
+#./migrate_postgresql_for_mac.sh ../i7.1z commit | /Library/PostgreSQL/12/bin/psql -p 5434  -d idempiereSeed8.2 > Seed/Seed_7.1z.lst
 ./migrate_postgresql_for_mac.sh ../i8.1 commit | /Library/PostgreSQL/12/bin/psql -p 5434  -d idempiereSeed8.2 > Seed/Seed_8.1.lst
 ./migrate_postgresql_for_mac.sh ../i8.1z commit | /Library/PostgreSQL/12/bin/psql -p 5434  -d idempiereSeed8.2 > Seed/Seed_8.1z.lst
 ./migrate_postgresql_for_mac.sh ../i8.2 commit | /Library/PostgreSQL/12/bin/psql -p 5434  -d idempiereSeed8.2 > Seed/Seed_8.2.lst
